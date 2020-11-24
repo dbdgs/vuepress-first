48b9340eefa3b177cea785a4ce8f98d3d9cc20db
Bravo Yeung <870878+geekplayers@user.noreply.gitee.com>
2020-11-24 22:41:25 +0800

    Fix issue.

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,16 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github
-echo 'b.dbdgs.cn' > CNAME
 if [ -z "$TOKEN_GITHUB" ]; then
   msg='deploy'
   githubUrl=git@github.com:dbdgs/dbdgs.github.io
 else
   msg='来自github actions的自动部署'
   githubUrl=https://dbdgs:${TOKEN_GITHUB}@github.com/dbdgs/dbdgs.github.io
-  git config --global user.name "DaBai"
-  git config --global user.email "legege007@yeah.net"
+  git config --local user.name "GitHub Action"
+  git config --local user.email "action@github.com"
 fi
+
 git init
 git add -A
 git commit -m "${msg}"
