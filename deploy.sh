2d0e301206d69e3cd8fe8ab8ad7525bbd2669469
Bravo Yeung <yangle20081982@163.com>
2020-11-25 09:58:32 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,20 +8,3 @@ npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
-
-# deploy to github
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:dbdgs/dbdgs.github.io
-else
-  msg='来自github actions的自动部署'
-  githubUrl=https://dbdgs:${GITHUB_TOKEN}@github.com/dbdgs/dbdgs.github.io
-fi
-
-git init
-git add -A
-git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github
-
-cd - # 退回开始所在目录
-rm -rf docs/.vuepress/dist
